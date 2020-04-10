b9f7bfcb2e389fd1aee689d087340f1c8ccd93f5
Paulo Calixto <klixto@outlook.com>
2020-04-10 10:04:58 -0300

    Componente global de ícones

---
+++ src/components/photons/Icon/Icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <img class="Icon" v-bind="$attrs" :src="src" @click="$emit('click')" />
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    path: {
+      type: String,
+      default: 'icons'
+    }
+  },
+  computed: {
+    src() {
+      return require(`@/assets/${this.path}/${this.name}.svg`)
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+// Photons
+import { Icon } from '@/components/photons'
+
+// Global Components
+Vue.component('Icon', Icon)
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
