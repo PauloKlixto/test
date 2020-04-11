2d96a2aa8ddf23b499d6b11dcfa0d9349256009c
Paulo Calixto <klixto@outlook.com>
2020-04-11 11:01:15 -0300

    Página de pagamento com integração ao DynamicForm

---
+++ src/views/payment/Payment.blueprint.js
@@ -0,0 +1,73 @@
+export default paymentOptions => [
+  {
+    id: 'card_number',
+    tag: 'InputGroup',
+    model: 'card_number',
+    rules: ['required'],
+    props: {
+      mask: 'card',
+      label: 'Número do cartão',
+      type: 'text',
+      floatingLabel: true,
+      invalidLabel: 'Número de cartão inválido'
+    },
+    events: {
+      input: 'getCardType'
+    }
+  },
+  {
+    id: 'card_name',
+    tag: 'InputGroup',
+    model: 'card_name',
+    rules: ['required'],
+    props: {
+      label: 'Nome (igual ao cartão)',
+      type: 'text',
+      floatingLabel: true,
+      invalidLabel: 'Insira seu nome completo'
+    }
+  },
+  {
+    id: 'card_date',
+    tag: 'InputGroup',
+    model: 'card_date',
+    rules: ['required'],
+    props: {
+      label: 'Validade',
+      type: 'number',
+      floatingLabel: true,
+      invalidLabel: 'Data inválida'
+    }
+  },
+  {
+    id: 'card_cvv',
+    tag: 'InputGroup',
+    model: 'card_cvv',
+    rules: ['required'],
+    props: {
+      label: 'CVV',
+      type: 'number',
+      floatingLabel: true,
+      invalidLabel: 'Código inválido'
+    }
+  },
+  {
+    id: 'card_installments',
+    tag: 'BaseSelect',
+    model: 'card_installments',
+    rules: ['required'],
+    props: {
+      label: 'Número de parcelas',
+      options: paymentOptions,
+      invalidLabel: 'insira o número de parcelas'
+    }
+  },
+  {
+    id: 'send',
+    tag: 'BaseButton',
+    props: {
+      label: 'Continuar',
+      type: 'submit'
+    }
+  }
+]
--- src/views/payment/Payment.vue
+++ src/views/payment/Payment.vue
@@ -11,26 +11,124 @@
         label="Adicione um novo cartão de crédito"
         icon="credit-card"
       />
-      <BaseCreditCard :form="form" />
+      <BaseCreditCard
+        :card-number="form.card_number"
+        :card-name="form.card_name"
+        :card-date="form.card_date"
+        :card-cvv="form.card_cvv"
+        :company="cardType.company"
+      />
     </section>
-    <section class="Payment__form">Form</section>
+    <section class="Payment__form">
+      <BaseSteps :steps="steps" class="Payment__steps" />
+      <DynamicForm
+        class="Payment__fields"
+        :model="form"
+        :blueprint="blueprint(paymentOptions)"
+        :endpoint-errors="endpointErrors"
+        @getCardType="getCardType"
+        @submit="onSubmit"
+      />
+    </section>
   </main>
 </template>
 
 <script>
-import { BaseBackButton, BaseCreditCard, BaseHeading } from '@/components/atoms'
+import {
+  BaseBackButton,
+  BaseCreditCard,
+  BaseHeading,
+  BaseSteps
+} from '@/components/atoms'
+import { DynamicForm } from '@/components/molecules'
+import blueprint from '@/views/payment/Payment.blueprint'
+import Rules from '@doc88/flux-validator-js'
+
 export default {
   name: 'Payment',
-  components: { BaseBackButton, BaseCreditCard, BaseHeading },
+  components: {
+    BaseBackButton,
+    BaseCreditCard,
+    BaseHeading,
+    BaseSteps,
+    DynamicForm
+  },
   data() {
     return {
       form: {
-        card_name: '',
         card_number: '',
-        card_month: '',
-        card_year: '',
-        card_cvv: ''
-      }
+        card_name: '',
+        card_date: '',
+        card_cvv: '',
+        card_installments: ''
+      },
+      endpointErrors: {},
+      cardType: {
+        company: '',
+        name: 'creditCardNumber',
+        valid: false
+      },
+      steps: [
+        {
+          id: 1,
+          step: 1,
+          name: 'Carrinho',
+          active: true
+        },
+        {
+          id: 2,
+          step: 2,
+          name: 'Pagamento',
+          active: false
+        },
+        {
+          id: 3,
+          step: 3,
+          name: 'Confirmação',
+          active: false
+        }
+      ],
+      paymentOptions: [
+        {
+          id: 0,
+          name: '12x R$1.000,00 sem juros'
+        },
+        {
+          id: 1,
+          name: '6x R$2.000,00 sem juros'
+        },
+        {
+          id: 2,
+          name: '3x R$4.000,00 sem juros'
+        },
+        {
+          id: 3,
+          name: '1x R$12.000,00 sem juros'
+        }
+      ]
+    }
+  },
+  methods: {
+    blueprint,
+
+    getCardType() {
+      this.cardType =
+        this.form.card_number === ''
+          ? {
+              company: '',
+              name: 'creditCardNumber',
+              valid: false
+            }
+          : Rules.creditCardNumber('creditCardNumber', this.form.card_number)
+    },
+
+    async onSubmit() {
+      console.log('enviou')
+
+      // const res = await payment.paymentData(this.form, (r) => r, (e) => e, () => this.setLoading(false))
+
+      // if (res.validation) return this.endpointErrors = res.validation
+      // if(!res.message) this.$router.push('/conclusion')
     }
   }
 }
@@ -62,8 +160,33 @@ export default {
   &__form {
     padding-top: $space-xl;
     padding-right: $space-xl;
-    padding-bottom: $space-xl;
+    padding-left: $space-xl;
     justify-self: end;
+    width: 100%;
+    max-width: 540px;
+    align-items: flex-end;
+    display: flex;
+    flex-flow: column;
+  }
+
+  &__steps {
+    margin-bottom: 40px;
+  }
+
+  &__fields {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 0.95fr 0.05fr 1fr;
+    align-items: self-end;
+    grid-template-rows: 36px;
+    grid-row-gap: 50px;
+    grid-column-gap: $space-md;
+    grid-template-areas:
+      'card_number card_number card_number'
+      'card_name card_name card_name'
+      'card_date card_date card_cvv'
+      'card_installments card_installments card_installments'
+      '. send send';
   }
 }
 </style>
