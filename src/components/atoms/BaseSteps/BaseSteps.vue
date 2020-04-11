ddad1a833da1dbcdb6e002bcdec3c7be7e930de9
Paulo Calixto <klixto@outlook.com>
2020-04-11 09:44:18 -0300

    Componente BaseSteps para os passos em cada página

---
+++ src/components/atoms/BaseSteps/BaseSteps.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="BaseSteps">
+    <section class="BaseSteps__list" v-for="item in steps" :key="item.id">
+      <Icon v-show="item.active" class="BaseSteps__icon" name="check" />
+      <span v-show="!item.active" class="BaseSteps__step">{{ item.step }}</span>
+      <span class="BaseSteps__name">{{ item.name }}</span>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'BaseSteps',
+  props: {
+    steps: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.BaseSteps {
+  font-size: 13px;
+  color: $color-red;
+  display: flex;
+
+  &__list {
+    position: relative;
+    padding-left: 50px;
+    display: flex;
+    align-items: center;
+    &:before {
+      //   content: '';
+      //   width: 8px;
+      //   height: 8px;
+      //   border-top: 2px solid $color-red;
+      //   border-left: 2px solid $color-red;
+      //   position: absolute;
+      @include chevronAlt($color-red);
+      left: 20px;
+      transform: rotateZ(135deg) translateY(0px);
+    }
+    &:first-child {
+      padding-left: 0;
+      &:before {
+        display: none;
+      }
+    }
+  }
+  &__icon {
+    display: flex;
+    border: 1px solid $color-red;
+    border-radius: 50%;
+    width: 13px;
+    height: 13px;
+    padding: 4px;
+    margin-right: 7.5px;
+    background-color: $color-red;
+  }
+  &__step {
+    display: flex;
+    border: 1px solid $color-red;
+    border-radius: 50%;
+    width: 22px;
+    height: 22px;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+    margin-right: 7.5px;
+  }
+  &__name {
+    display: block;
+  }
+}
+</style>
